04a959eef30138e69100ae5c51108a9f70b09d31
Sky12072 <12072@coderacademy.edu.au>
2022-12-07 22:05:08 +1100

    added material ui and styled comp

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav"
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <h1>Hi !</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Nav() {
+
+
+    return (
+        <nav>
+            <a href="/">Home</a>
+            <a href="/">About </a>
+            <a href="/">Sign up </a>
+            <a href="/">Sign in </a>
+            
+        </nav>
+    )
+}
